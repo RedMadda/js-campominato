d7c30b4a1e4ffe14446ceb802db9c4e37a48fb7e
RedMadda <maddalenarossaro91@gmail.com>
2021-06-08 17:44:36 +0200

    fine base

--- js/script.js
+++ js/script.js
@@ -1,35 +1,48 @@
 // Consegna
 // Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
 var numsPc = [];
-pushNums(numsPc, 16, 1, 100);
+pushNumsPc(numsPc, 16, 1, 100);
 console.log(numsPc);
 
 // In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
-
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 var numsUser = [];
-while (numsUser.length < 5){
-    var numUser = parseInt(prompt("Inserisci un numero compreso fra 1 e 100."));
-    do{
-        numUser = parseInt(prompt("Attenzione! Il numero scelto deve essere positivo, fra 1 e 100 e non già utilizzato. Riprova!"));
-    } while (numUser <= 0 || numsUser.includes(numUser)  );
-    numsUser.push(numUser);
-     console.log(numUser);
- }
- console.log(numsUser);
+pushNumsUser(numsUser, 5, numsPc);
+
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+ 
+
 // FUNZIONI
 
-function pushNums(array, t, min, max){
-while (array.length < t){
+function pushNumsPc(array1, t, min, max){
+while (array1.length < t){
     var num = getRndNum(min, max);
-    if (!array.includes(num)){
-        array.push(num);
-        }
+    if (!array1.includes(num)){
+        array1.push(num);
+    }
     }
 }
 function getRndNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
-  }
+}
 
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+function pushNumsUser(array1, t, array2){
+    var lighter = true;
+    while (array1.length < t && lighter){
+    do{
+        var numUser = parseInt(prompt("Attenzione! Scegli un numero positivo, fra 1 e 100 e non già utilizzato."));
+    } while (numUser <= 0 || numsUser.includes(numUser)  );
+
+    if (array2.includes(numUser)){
+        alert("Mi dispiace, hai perso!");
+        console.log("Numero tentativi: " + array1.length);
+        lighter = false;
+    }
+    console.log(numsUser);
+    array1.push(numUser);
+    }
+}
+
+
  
