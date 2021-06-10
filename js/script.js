cb848835951558ed241a4d92d66090d5e0c8593b
RedMadda <maddalenarossaro91@gmail.com>
2021-06-10 08:01:43 +0200

    pazzie

--- js/script.js
+++ js/script.js
@@ -1,48 +1,150 @@
-// Consegna
-// Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati.
-var numsPc = [];
-pushNumsPc(numsPc, 16, 1, 100);
-console.log(numsPc);
+ // Get the modal
+var modal = document.getElementById("myModal");
 
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. L’utente non può inserire più volte lo stesso numero.
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-var numsUser = [];
-pushNumsUser(numsUser, 5, numsPc);
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
- 
-
-// FUNZIONI
-
-function pushNumsPc(array1, t, min, max){
-while (array1.length < t){
-    var num = getRndNum(min, max);
-    if (!array1.includes(num)){
-        array1.push(num);
-    }
-    }
-}
-function getRndNum(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
 }
 
-function pushNumsUser(array1, t, array2){
-    var lighter = true;
-    while (array1.length < t && lighter){
-    do{
-        var numUser = parseInt(prompt("Attenzione! Scegli un numero positivo, fra 1 e 100 e non già utilizzato."));
-    } while (numUser <= 0 || numsUser.includes(numUser)  );
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-    if (array2.includes(numUser)){
-        alert("Mi dispiace, hai perso!");
-        console.log("Numero tentativi: " + array1.length);
-        lighter = false;
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// ***game
+
+new Vue({
+  el : '#app',
+  template : `
+  <flower-game 
+    id="game"
+    :duration="100"
+    :speed="5"
+    :width="800"
+    :height="600"
+  /> 
+`,
+  components : {
+    FlowerGame
+  } 
+
+  // flowerGame Component [game logic, no scoreboard and also no display the flowers on screen--> delegate to other components to cleaness]:
+  
+
+
+})
+
+
+
+
+data () {
+  return {
+    started : false,
+    currentScore : 0,
+    timeRemaining : 0,
+    flowerId : 0,
+    timerId : null,
+    liveFlowers : {},
+    flowerTimerId : null
+  }
+}
+
+// game component
+const FlowerGame = {
+  
+  components : {
+    Flower,
+    ScoreBoard
+  }
+
+
+
+
+// take html id app
+new Vue({
+  el : '#app',
+  template : `
+  <flower-game
+    id="game"
+    :duration="100"
+    :speed="5"
+    :width="800"
+    :height="600"
+  />
+`,
+  components : {
+    FlowerGame
+  }
+})
+
+// dumb child flower component, passing infos to the parent
+
+const Flower = {
+  
+  props : {
+    flower : {
+      type : Object,
+      required : true
+     }
+  }
+  
+  methods : {
+    
+    flowerClicked () {
+      this.$emit('clicked');
     }
-    console.log(numsUser);
-    array1.push(numUser);
+    
+  }
+  
+  template : `
+  <button 
+    :style="{ left: flower.x + 'px', top : flower.y + 'px'}"
+    @click="flowerClicked"
+    class="flower  btn">🌸
+  </button>
+`
+  
+}
+
+// score board
+
+const ScoreBoard = {
+  
+  props : {
+    score : {
+      type : Number,
+      required : true
+    },
+    timeRemaining : {
+      type : Number,
+      required : true
     }
+  }
+  
+  template :
+  <div class="scoreboard">
+
+    <span>Score: {{ score }}</span>
+    <span>Time: {{ timeRemaining }}</span>
+
+  </div>
+
 }
 
 
- 
+
+}
+
